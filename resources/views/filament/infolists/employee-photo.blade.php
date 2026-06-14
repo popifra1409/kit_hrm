fc2a83df29f61220d048a3fba4108b89458ca908
popifra1409 <porio.peyie@gmail.com>
2026-06-14 14:02:52 +0100

    recqdrqge de photo

--- resources/views/filament/infolists/employee-photo.blade.php
+++ resources/views/filament/infolists/employee-photo.blade.php
@@ -1,9 +1,9 @@
-<div class="flex items-center justify-center">
+<div class="flex items-center justify-center p-6">
     @if($record->photo)
         <img src="{{ Storage::url($record->photo) }}" alt="Photo de {{ $record->full_name }}"
-            class="w-32 h-32 rounded-full border-4 border-gray-300 shadow-lg object-cover">
+            class="w-80 h-80 rounded-lg border-4 border-gray-300 shadow-2xl object-cover">
     @else
         <img src="{{ url('/images/default-avatar.png') }}" alt="Avatar par défaut"
-            class="w-32 h-32 rounded-full border-4 border-gray-300 shadow-lg object-cover bg-gray-200">
+            class="w-80 h-80 rounded-lg border-4 border-gray-300 shadow-2xl object-cover bg-gray-200">
     @endif
 </div>
